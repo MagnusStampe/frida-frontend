b65f91bc578783c8204f8a035672ce26bc145b06
Magnus stampe Nielsen <m_s_nielsen288@hotmail.com>
2019-12-09 13:20:20 +0100

    initialcommit

---
+++ includes/top.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>FRIDA | PAGE</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<header id="page_header">
+    <img id="header_logo">
+    <button id="burger_button">
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+    </button>
+    <nav class="header_nav">
+        <a href="<?= __DIR__ ?>/../inventory.php">Inventory</a>
+        <a href="<?= __DIR__ ?>/../recipes.php">Recipes</a>
+        <a href="<?= __DIR__ ?>/../shopping-list.php">Shopping list</a>
+    </nav>
+</header>
+<body>
---
+++ inventory.php
@@ -0,0 +1,3 @@
+<?php require_once(__DIR__.'/includes/top.php'); ?>
+
+<?php require_once(__DIR__.'/includes/bottom.php'); ?>
